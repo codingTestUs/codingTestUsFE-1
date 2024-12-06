5988834868567ab9e47304d0521530c07f858302
breath02 <joon7545@gmail.com>
2024-12-06 14:25:12 +0900

    rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; // React 플러그인
+import path from 'path';
+import viteCompression from 'vite-plugin-compression'; // 압축 플러그인
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression(), // 압축 플러그인 추가
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // @를 src 경로로 설정
     },
   },
   server: {
-    port: 3000, // Set the port to 3000
-  }
-})
+    port: 3000, // 서버 포트를 3000으로 설정
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // React 관련 패키지를 분리
+        },
+      },
+    },
+  },
+});
